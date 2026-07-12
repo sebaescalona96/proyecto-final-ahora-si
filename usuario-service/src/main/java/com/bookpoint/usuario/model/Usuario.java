03268e814168a05358a5f0ed4f6e4e827ce92b9e
sebaescalona96 <sebaescalona96@gmail.com>
2026-07-11 21:09:32 -0400

    /update usuario-model

--- usuario-service/src/main/java/com/bookpoint/usuario/model/Usuario.java
+++ usuario-service/src/main/java/com/bookpoint/usuario/model/Usuario.java
@@ -1,13 +1,14 @@
 package com.bookpoint.usuario.model;
+
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
-@Data @NoArgsConstructor @AllArgsConstructor
-@Entity @Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
